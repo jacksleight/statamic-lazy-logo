4d79955d424a76e29e0b57d12985a46a398c4f47
Jack Sleight <jacksleight@gmail.com>
2021-09-21 12:23:44 +0100

    Default font weight to 500

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.2.1 (2021-09-21)
+
+- [improved] `font_weight` now defaults to `500`
+
 ## 0.2.0 (2021-09-21)
 
 - [break] `gradient` config value is now merged with `color`, just specify an array instead of a single value
--- config/lazy-logo.php
+++ config/lazy-logo.php
@@ -6,6 +6,6 @@ return [
     'color'       => env('LAZY_LOGO_COLOR', '#19292f'), // Use an array for a gradient: ['#f94f57', '#c471ed', '#12c2e9']
     'font_family' => 'Inter UI,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue',
     'font_size'   => '22px',
-    'font_weight' => 'normal',
+    'font_weight' => '500',
 
 ];
