50485adfa93101dac067a1f1ad10ce8578068ad8
Jack Sleight <jacksleight@gmail.com>
2022-12-12 12:39:56 +0000

    WIP

--- routes/cp.php
+++ routes/cp.php
@@ -3,11 +3,12 @@
 use JackSleight\StatamicLazyLogo\Http\Controllers\CP\AssetController;
 
 Route::group([
+    'prefix' => 'lazy-logo',
     'controller' => AssetController::class,
     'middleware' => ['cache.headers:max_age=31536000'],
     'excluded_middleware' => ['statamic.cp.authenticated'],
 ], function () {
-    Route::get('lazy-logo/nav.svg', 'svg')->defaults('type', 'nav');
-    Route::get('lazy-logo/outside.svg', 'svg')->defaults('type', 'outside');
-    Route::get('lazy-logo/cp.css', 'css');
+    Route::get('nav.svg', 'svg')->defaults('type', 'nav');
+    Route::get('outside.svg', 'svg')->defaults('type', 'outside');
+    Route::get('cp.css', 'css');
 });
